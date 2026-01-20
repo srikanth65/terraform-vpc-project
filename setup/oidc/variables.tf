b1ca6309cab9cc4bd50393fba2f8a73d76e9d690
srikanth65 <srikanthch0465@gmail.com>
2026-01-20 15:02:05 -0800

    fix: update OIDC trust policy

--- setup/oidc/variables.tf
+++ setup/oidc/variables.tf
@@ -1,7 +1,9 @@
 variable "github_repository" {
   description = "GitHub repository in format 'owner/repo'"
   type        = string
+  default     = "*/terraform-vpc-project"
   # Example: "your-username/terraform-vpc-project"
+  # Use wildcard "*" for username if you want to allow any owner
 }
 
 variable "aws_region" {
